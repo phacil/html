cc4a2f41d181fd237b365ecac11a5957f9d1857e
Alisson Nascimento <alisson.sa.nascimento@gmail.com>
2016-12-14 02:21:26 -0300

    1.0.1 fix Form var data

--- src/Form.php
+++ src/Form.php
@@ -11,7 +11,7 @@ class Form extends HTML{
     private static $data = [];
     
     public static function __callStatic($name, $arguments=[]) {
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         if(empty($arguments)){$arguments[0]='';}
         if(isset($data[$arguments[0]])){
             $arguments[0] = $data[$arguments[0]];
@@ -53,7 +53,7 @@ class Form extends HTML{
     }
     
     private static function __input($elementObject, $campo){  
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         $elementObject->value('');
         if(isset($data[$campo])){
             $elementObject->value($data[$campo]);
@@ -75,7 +75,7 @@ class Form extends HTML{
     }
     
     public static function select($campo = null, $options = null, $empty=false){        
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         $selected = null;
         if(isset($data[$campo])){
             $selected = $data[$campo];
@@ -88,7 +88,7 @@ class Form extends HTML{
     
     public static function textarea($campo){
         $elementObject = new FormElement('textarea');
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         $elementObject->text('');
         if(isset($data[$campo])){
             $elementObject->text($data[$campo]);
@@ -99,7 +99,7 @@ class Form extends HTML{
     }
     
     public static function radio($campo = null, $list = null, $checked = null) {
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         $selected = null;
         if(isset($data[$campo])){
             $checked = $data[$campo];
@@ -111,7 +111,7 @@ class Form extends HTML{
     }
     
     public static function checkbox($campo = null, $list = null, $checked = null) {
-        $data = \Phacil\HTTP\Request::getData();
+        $data = \Phacil\Component\HTTP\Request::getData();
         $selected = null;
         if(isset($data[$campo])){
             $checked = array_keys($data[$campo]);
