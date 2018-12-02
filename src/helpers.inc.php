eefac5883eb03f83040c6307774df85417857fcb
Alisson Nascimento <alisson.nascimento@gmail.com>
2018-12-01 23:35:14 -0300

    composer, phacil_html, phacil_form

--- src/helpers.inc.php
+++ src/helpers.inc.php
@@ -23,3 +23,27 @@ function form()
     }
     return new \Phacil\HTML\Form();
 }
+
+/**
+ * 
+ * @return \Phacil\HTML\HTML
+ */
+function phacil_html()
+{
+    if(!is_null(\Phacil\HTML\HTML::getInstance())){
+        return \Phacil\HTML\HTML::getInstance();
+    }
+    return new \Phacil\HTML\HTML();
+}
+
+/**
+ * 
+ * @return \Phacil\HTML\Form
+ */
+function phacil_form()
+{
+    if(!is_null(\Phacil\HTML\Form::getInstance())){
+        return \Phacil\HTML\Form::getInstance();
+    }
+    return new \Phacil\HTML\Form();
+}
