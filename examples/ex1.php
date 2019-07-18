b49d10a6848f542e498eddc8bfba500dc70afb62
Alisson Nascimento <alisson.nascimento@gonubetec.com.br>
2019-07-18 13:58:52 -0300

    funcoes html form num namesapace phacil

--- examples/ex1.php
+++ examples/ex1.php
@@ -3,11 +3,11 @@
  * @property HTMLElement $html
  */
 include '../vendor/autoload.php';
-use Phacil\HTML\HTML;
+use phacil\html\HTML;
 use Phacil\HTML\HTMLElement;
 
 /* You can echo this code */
-echo html()->h1('This is a title');
+echo phacil\html()->h1('This is a title');
 /* or call output() function */
 HTML::h2('This is a second title')->output();
 /* and use chain mode to set attributes */
--- src/helpers.inc.php
+++ src/helpers.inc.php
@@ -1,49 +1,51 @@
 <?php
-
-/**
- * 
- * @return \Phacil\HTML\HTML
- */
-function html()
-{
-    if(!is_null(\Phacil\HTML\HTML::getInstance())){
-        return \Phacil\HTML\HTML::getInstance();
+namespace phacil{
+    /**
+     * 
+     * @return \Phacil\HTML\HTML
+     */
+    function html()
+    {
+        if(!is_null(\Phacil\HTML\HTML::getInstance())){
+            return \Phacil\HTML\HTML::getInstance();
+        }
+        return new \Phacil\HTML\HTML();
     }
-    return new \Phacil\HTML\HTML();
+
+    /**
+     * 
+     * @return \Phacil\HTML\Form
+     */
+    function form()
+    {
+        if(!is_null(\Phacil\HTML\Form::getInstance())){
+            return \Phacil\HTML\Form::getInstance();
+        }
+        return new \Phacil\HTML\Form();
+    }
 }
 
-/**
- * 
- * @return \Phacil\HTML\Form
- */
-function form()
-{
-    if(!is_null(\Phacil\HTML\Form::getInstance())){
-        return \Phacil\HTML\Form::getInstance();
-    }
-    return new \Phacil\HTML\Form();
-}
 
-/**
- * 
- * @return \Phacil\HTML\HTML
- */
-function phacil_html()
-{
-    if(!is_null(\Phacil\HTML\HTML::getInstance())){
-        return \Phacil\HTML\HTML::getInstance();
-    }
-    return new \Phacil\HTML\HTML();
-}
+// /**
+//  * 
+//  * @return \Phacil\HTML\HTML
+//  */
+// function phacil_html()
+// {
+//     if(!is_null(\Phacil\HTML\HTML::getInstance())){
+//         return \Phacil\HTML\HTML::getInstance();
+//     }
+//     return new \Phacil\HTML\HTML();
+// }
 
-/**
- * 
- * @return \Phacil\HTML\Form
- */
-function phacil_form()
-{
-    if(!is_null(\Phacil\HTML\Form::getInstance())){
-        return \Phacil\HTML\Form::getInstance();
-    }
-    return new \Phacil\HTML\Form();
-}
+// /**
+//  * 
+//  * @return \Phacil\HTML\Form
+//  */
+// function phacil_form()
+// {
+//     if(!is_null(\Phacil\HTML\Form::getInstance())){
+//         return \Phacil\HTML\Form::getInstance();
+//     }
+//     return new \Phacil\HTML\Form();
+// }
